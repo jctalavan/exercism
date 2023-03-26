dfe6d13e0a7e60754b7cced12134700bc7e849be
jctalavan <jctalavan8@gmail.com>
2023-03-26 10:46:10 +0200

    Exercise 6

---
+++ tests/6.resistor-color.test.ts
@@ -0,0 +1,30 @@
+import { colorCode, COLORS } from '../src/6.resistor-color'
+
+describe('color code', () => {
+  test('Black', () => {
+    expect(colorCode('black')).toEqual(0)
+  })
+  test('White', () => {
+    expect(colorCode('white')).toEqual(9)
+  })
+  test('Orange', () => {
+    expect(colorCode('orange')).toEqual(3)
+  })
+})
+
+describe('Colors', () => {
+  test('returns all colors', () => {
+    expect(COLORS).toEqual([
+      'black',
+      'brown',
+      'red',
+      'orange',
+      'yellow',
+      'green',
+      'blue',
+      'violet',
+      'grey',
+      'white',
+    ])
+  })
+})
